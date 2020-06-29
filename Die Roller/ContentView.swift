0654a19ed0d9ad649159cd12ef8ca68036cb01c6
Sam <samgauck@icloud.com>
2020-06-29 16:16:37 -0400

    Initial Commit

---
+++ Die Roller/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Die Roller
+//
+//  Created by Sam Gauck on 6/29/20.
+//  Copyright © 2020 Sam Gauck. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
