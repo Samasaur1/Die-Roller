4a580d4e5abc0154ccd3dd567f73cc27a8405832
Sam <30577766+Samasaur1@users.noreply.github.com>
2020-07-12 15:25:40 -0400

    Add drag&drop (#1)
    * WIP
    
    * WIP 2
    
    * Technically functional drag&drop
    
    The coordinates do not convert correctly between global and scene
    
    * Change default point back to the center
    
    * I'm afraid we need to use … math
    
    fyi, that's a Futurama reference ("The Prisoner of Benda")
    Use math to make drag&drop work in the correct location
    
    * Allow removing dice by dragging off to the left
    
    * remove unused dependency on Combine

--- Die Roller/ContentView.swift
+++ Die Roller/ContentView.swift
@@ -12,7 +12,10 @@ import SpriteKit
 
 struct ContentView: View {
     @State var sidebar = false
-    @State var dice: [Die] = []
+    @State var diceWithMetadata: [(Die, CGPoint?, SKShapeNode?)] = []
+    var dice: [Die] {
+        diceWithMetadata.map { $0.0 }
+    }
     @State var modifiers: [Int] = []
     @State var presentingRoll = false
     @State var rollResult: Roll = 0
@@ -32,102 +35,130 @@ struct ContentView: View {
         Dice(dice: self.dice, withModifier: self.modifiers.reduce(0, +))
     }
     var body: some View {
-        VStack(spacing: 0) {
-            GeometryReader { (geo: GeometryProxy) in
-                HStack(spacing: 0) {
-                    if self.sidebar {
-                        VStack {
-                            List {
-                                ForEach([4, 6, 8, 10, 12, 20, 100], id: \.self) { sides in
+        ZStack {
+            VStack(spacing: 0) {
+                GeometryReader { (geo: GeometryProxy) in
+                    HStack(spacing: 0) {
+                        if self.sidebar {
+                            VStack {
+                                List {
+                                    ForEach([4, 6, 8, 10, 12, 20, 100], id: \.self) { sides in
+                                        HStack {
+                                            Text("d\(sides)")
+                                            Spacer()
+                                            Button(action: {
+                                                self.add(sides)
+                                                self.addingCustomDie = false
+                                                self.addingModifier = false
+                                            }) {
+                                                Image(systemName: "plus")
+                                            }
+                                        }.gesture(self.dragDieGesture(sides: sides))
+                                    }
                                     HStack {
-                                        Text("d\(sides)")
+                                        Text("Custom Die")
                                         Spacer()
                                         Button(action: {
-                                            self.add(sides)
-                                            self.addingCustomDie = false
+                                            self.addingCustomDie = true
                                             self.addingModifier = false
                                         }) {
                                             Image(systemName: "plus")
                                         }
                                     }
-                                }
-                                HStack {
-                                    Text("Custom Die")
-                                    Spacer()
-                                    Button(action: {
-                                        self.addingCustomDie = true
-                                        self.addingModifier = false
-                                    }) {
-                                        Image(systemName: "plus")
+                                    HStack {
+                                        Text("Modifier")
+                                        Spacer()
+                                        Button(action: {
+                                            self.addingModifier = true
+                                            self.addingCustomDie = false
+                                        }) {
+                                            Image(systemName: "plus")
+                                        }
                                     }
                                 }
-                                HStack {
-                                    Text("Modifier")
-                                    Spacer()
-                                    Button(action: {
-                                        self.addingModifier = true
-                                        self.addingCustomDie = false
-                                    }) {
-                                        Image(systemName: "plus")
+                            }.frame(width: geo.size.width / 4, height: nil, alignment: .leading).transition(.asymmetric(insertion: .slide, removal: .move(edge: .leading)))
+                        }
+                        Divider()
+                        ZStack {
+                            SceneView(scene: DieScene(), width: geo.size.width, height: geo.size.height, dice: self.$diceWithMetadata, modifiers: self.$modifiers)
+                            HStack {
+                                Button(action: {
+                                    withAnimation {
+                                        self.sidebar.toggle()
                                     }
-                                }
+                                }) {
+                                    Image(systemName: "chevron.\(self.sidebar ? "left" : "right")")
+                                        .padding(.vertical, nil)
+                                        .padding(.trailing, 7.5)
+                                        .padding(.leading, self.sidebar ? 5 : 2)
+                                        .background(Color.gray)
+                                }.highPriorityGesture(self.sidebarDragGesture)
+                                Spacer()
                             }
-                        }.frame(width: geo.size.width / 4, height: nil, alignment: .leading).transition(.asymmetric(insertion: .slide, removal: .move(edge: .leading)))
-                    }
-                    Divider()
-                    ZStack {
-                        SceneView(scene: DieScene(), width: geo.size.width, height: geo.size.height, dice: self.$dice, modifiers: self.$modifiers)
-                        HStack {
-                            Button(action: {
-                                withAnimation {
-                                    self.sidebar.toggle()
-                                }
-                            }) {
-                                Image(systemName: "chevron.\(self.sidebar ? "left" : "right")")
-                                    .padding(.vertical, nil)
-                                    .padding(.trailing, 7.5)
-                                    .padding(.leading, self.sidebar ? 5 : 2)
-                                    .background(Color.gray)
-                            }.highPriorityGesture(self.sidebarDragGesture)
-                            Spacer()
-                        }
-                        if self.addingCustomDie {
-                            self.addingCustomDieView
-                        }
-                        if self.addingModifier {
-                            self.addingModifierView
+                            if self.addingCustomDie {
+                                self.addingCustomDieView
+                            }
+                            if self.addingModifier {
+                                self.addingModifierView
+                            }
                         }
                     }
+                }
+                Divider()
+                HStack {
+                    Button(action: {
+                        self.writingDieString = true
+                        self.addingCustomDie = false
+                        self.addingModifier = false
+                        self.presentingRoll = false
+                    }) {
+                        Text(diceObj.debugDescription)
+                    }.foregroundColor(.primary)
+                        .popover(isPresented: $writingDieString) {
+                            self.writingDieStringView
+                    }
+                    Spacer()
+                    Button(action: {
+                        self.roll()
+                    }) {
+                        Text("Roll!")
+                    }.popover(isPresented: $presentingRoll) {
+                            self.rollPresentationView
+                    }
+                    Button(action: {
+                        self.diceWithMetadata.forEach { $0.2?.removeFromParent() }
+                        self.diceWithMetadata = []
+                        self.modifiers = []
+                    }) {
+                        Text("Clear").foregroundColor(.red)
+                    }
+                }.padding()
+            }
+            if drag.ing {
+                GeometryReader { _ in
+                    Circle().fill(Color.green).frame(width: 10, height: 10, alignment: .center).position(self.drag.location)
                 }
             }
-            Divider()
-            HStack {
-                Button(action: {
-                    self.writingDieString = true
-                    self.addingCustomDie = false
-                    self.addingModifier = false
-                    self.presentingRoll = false
-                }) {
-                    Text(diceObj.debugDescription)
-                }.foregroundColor(.primary)
-                    .popover(isPresented: $writingDieString) {
-                        self.writingDieStringView
-                }
-                Spacer()
-                Button(action: {
-                    self.roll()
-                }) {
-                    Text("Roll!")
-                }.popover(isPresented: $presentingRoll) {
-                        self.rollPresentationView
-                }
-                Button(action: {
-                    self.dice = []
-                    self.modifiers = []
-                }) {
-                    Text("Clear").foregroundColor(.red)
-                }
-            }.padding()
+        }
+    }
+
+    @State private var drag: Drag = Drag()
+
+    internal struct Drag {
+        internal init(ing: Bool, location: CGPoint, sides: Int) {
+            self.ing = ing
+            self.location = location
+            self.sides = sides
+        }
+
+        var ing: Bool
+        var location: CGPoint
+        var sides: Int
+
+        internal init() {
+            self.ing = false
+            self.location = .zero
+            self.sides = 1
         }
     }
 
@@ -140,6 +171,27 @@ struct ContentView: View {
         } catch {
             return (true, error.localizedDescription)
         }
+    }
+
+    func dragDieGesture(sides: Int) -> some Gesture {
+        return DragGesture(coordinateSpace: .global)
+            .onChanged { val in
+                self.drag.ing = true
+                self.drag.location = val.location
+                self.drag.sides = sides
+            }.onEnded { val in
+                self.drag.ing = false
+                print("generating d\(sides) at \(val.location) (global coords)")
+                let d = try! Die(sides: sides)
+                let w = UIScreen.main.bounds.size.width
+                let loc = CGPoint(x: val.location.x - (w / 4), y: val.location.y)
+                //i thought i'd need to divide by 3w/4, but that didn't work AT ALL
+                print("corressponds to \(loc)")
+                guard loc.x > 0 else {
+                    return
+                }
+                self.diceWithMetadata.append((d, loc, nil))
+            }
     }
 
     private var sidebarDragGesture: some Gesture {
@@ -282,10 +334,11 @@ struct ContentView: View {
                     Spacer()
                     Button(action: {
                         let dice = try! Dice(self.dieString)
-                        self.dice = []
+                        self.diceWithMetadata.forEach { $0.2?.removeFromParent() }
+                        self.diceWithMetadata = []
                         for (die, count) in dice.dice {
                             for _ in 0..<count {
-                                self.dice.append(die)
+                                self.diceWithMetadata.append((die, nil, nil))
                             }
                         }
                         self.modifiers = dice.modifier == 0 ? [] : [dice.modifier]
@@ -323,7 +376,7 @@ struct ContentView: View {
             die = try! Die(sides: sides)
         }
 
-        self.dice.append(die)
+        self.diceWithMetadata.append((die, nil, nil))
         self.customDieSidesStr = "1"
     }
 
@@ -339,7 +392,7 @@ struct ContentView: View {
 struct SceneView: UIViewRepresentable {
     let scene: DieScene
 
-    init(scene: DieScene, width: CGFloat, height: CGFloat, dice: Binding<[Die]>, modifiers: Binding<[Int]>) {
+    init(scene: DieScene, width: CGFloat, height: CGFloat, dice: Binding<[(Die, CGPoint?, SKShapeNode?)]>, modifiers: Binding<[Int]>) {
         self.scene = scene
         scene.size = .init(width: width, height: height)
         scene.anchorPoint = .init(x: 0.5, y: 0.5)
@@ -361,9 +414,8 @@ struct SceneView: UIViewRepresentable {
 }
 
 class DieScene: SKScene {
-    var dice: Binding<[Die]>! = nil
+    var dice: Binding<[(Die, CGPoint?, SKShapeNode?)]>! = nil
     var modifiers: Binding<[Int]>! = nil
-    private var diceNodes: [(Die, SKShapeNode)] = []
     private var modifierNodes: [(Int, SKLabelNode)] = []
 
     private var selected: SKNode? = nil
@@ -374,32 +426,34 @@ class DieScene: SKScene {
     }
 
     override func update(_ currentTime: TimeInterval) {
-        if dice.wrappedValue.count > diceNodes.count {
-            add(die: dice.wrappedValue.last!)
-        } else if dice.wrappedValue.count < diceNodes.count {
-            if dice.wrappedValue.isEmpty {
-                diceNodes.forEach { (_, node) in
-                    node.removeFromParent()
-                    node.removeAllChildren()
+        for i in dice.wrappedValue.indices {
+            if let node = dice.wrappedValue[i].2 {
+                if self.children.contains(node) {
+                    continue
+                } else {
+                    addChild(node)
                 }
-                diceNodes = []
-            }
-            if let idx = Array(zip(dice.wrappedValue, diceNodes)).firstIndex(where: { $0.0 != $0.1.0 }) {
-                diceNodes[idx].1.removeFromParent()
-                diceNodes[idx].1.removeAllChildren()
-                diceNodes.remove(at: idx)
+            } else {
+                let n = dieNode(for: dice.wrappedValue[i].0.sides)
+                n.position = self.convertPoint(fromView: dice.wrappedValue[i].1 ?? self.convertPoint(toView: .zero))
+                addChild(n)
+                dice.wrappedValue[i].2 = n
             }
         }
-        for i in 0..<diceNodes.count {
-            if dice.wrappedValue[i] == diceNodes[i].0 { continue }
-            let pos = diceNodes[i].1.position
-            diceNodes[i].1.removeFromParent()
-            diceNodes[i].1.removeAllChildren()
-            let n = dieNode(for: dice.wrappedValue[i].sides)
-            addChild(n)
-            n.position = pos
-            diceNodes[i] = (dice.wrappedValue[i], n)
-        }
+//        for (die, pos, node) in dice.wrappedValue {
+//            if let node = node {
+//                if self.children.contains(node) {
+//                    continue
+//                } else {
+//                    addChild(node)
+//                }
+//            } else {
+//                let n = dieNode(for: die.sides)
+//                n.position = pos ?? .zero
+//                addChild(n)
+//                node = n
+//            }
+//        }
 
         if modifiers.wrappedValue.count > modifierNodes.count {
             add(modifier: modifiers.wrappedValue.last!)
@@ -428,17 +482,30 @@ class DieScene: SKScene {
             modifierNodes[i] = (modifiers.wrappedValue[i], n)
         }
 
-        for (i, (_, node)) in diceNodes.enumerated() {
+        for (i, (_, _, node)) in dice.wrappedValue.enumerated() {
+            guard let node = node else {
+                continue
+            }
             if (-self.frame.height / 2) > (node.position.y + node.frame.height/2) {
                 if node == selected { continue }
                 node.removeFromParent()
                 dice.wrappedValue.remove(at: i)
-                diceNodes.remove(at: i)
+            }
+            if (node.position.x + node.frame.width/2) < (-self.frame.width / 2) {
+                if node == selected { continue }
+                node.removeFromParent()
+                dice.wrappedValue.remove(at: i)
             }
         }
 
         for (i, (_, node)) in modifierNodes.enumerated() {
             if (-self.frame.height / 2) > (node.position.y + node.frame.height/2) {
+                if node == selected { continue }
+                node.removeFromParent()
+                modifiers.wrappedValue.remove(at: i)
+                modifierNodes.remove(at: i)
+            }
+            if (node.position.x + node.frame.width/2) < (-self.frame.width / 2) {
                 if node == selected { continue }
                 node.removeFromParent()
                 modifiers.wrappedValue.remove(at: i)
@@ -499,12 +566,6 @@ class DieScene: SKScene {
             n.addChild(label)
         }
         return n
-    }
-
-    func add(die: Die) {
-        let n = dieNode(for: die.sides)
-        addChild(n)
-        diceNodes.append((die, n))
     }
 
     private func modifierNode(for modifier: Int) -> SKLabelNode {
